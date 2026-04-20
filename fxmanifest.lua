534f58da8f58c84ff7e959e4cd92b4bb16e1c26f
Guardian <72229591+TehGuardian@users.noreply.github.com>
2026-04-20 13:05:02 -0400

    Bump version from 1.0.0 to 1.0.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,12 +2,10 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-version '1.0.0'
+version '1.0.1'
 repository 'https://github.com/Mythic-Framework/mythic-voip'
 client_script "@mythic-base/components/cl_error.lua"
 client_script "@mythic-pwnzor/client/check.lua"
-
-
 
 client_scripts {
     'shared/**/*.lua',
